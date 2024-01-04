4ae5ad29d9f2dcba7fc29f4083efaa0c1d9de9a9
Aura Kelloniemi <kaura.dev@sange.fi>
2024-01-04 23:30:12 +0200

    Turn `lovegen` into a library
    This is to make lovegen a reusable website generator library.

---
+++ src/LoveGen.hs
@@ -0,0 +1,9 @@
+-- |
+-- Module      : LoveGen
+-- Description : A static website generator support library
+-- Copyright   : Copyright (C) 2023-2024 Aura Kelloniemi
+-- License     : GPL-3
+-- Maintainer  : kaura.dev@sange.fi
+-- Stability   : experimental
+-- Portability : GHC
+module LoveGen () where
